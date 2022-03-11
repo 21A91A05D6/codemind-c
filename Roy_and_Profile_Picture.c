d985f7a10382b86a7e017a595cc00292605c1b2e
21A91A05D6 <97495941+21A91A05D6@users.noreply.github.com>
2022-03-11 10:21:43 +0530

    #include<stdio.h> int main() {     int w,h,l,t;     scanf("%d",&l);     scanf("%d",&t);     for(;t>0;--t)     {         scanf("%d%d",&w,&h);         if(w<l || h<l)         {             printf("UPLOAD ANOTHER ");         }         else if(w>=l && h>=l)         {             if(w==h)             {                 printf("ACCEPTED ");             }             else             {                 printf("CROP IT ");             }         }
        }
    }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int w,h,l,t;
+    scanf("%d",&l);
+    scanf("%d",&t);
+    for(;t>0;--t)
+    {
+        scanf("%d%d",&w,&h);
+        if(w<l || h<l)
+        {
+            printf("UPLOAD ANOTHER
+");
+        }
+        else if(w>=l && h>=l)
+        {
+            if(w==h)
+            {
+                printf("ACCEPTED
+");
+            }
+            else
+            {
+                printf("CROP IT
+");
+            }
+        }
+        
+    }
+}
