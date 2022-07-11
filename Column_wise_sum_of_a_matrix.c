03777d52abba3848a6cdc1af6ae0ac7b255b4e61
21A91A05D6 <97495941+21A91A05D6@users.noreply.github.com>
2022-07-11 10:59:11 +0530

    #include <stdio.h> int main() {      int a[100][100],i,n,m,j,sum=0,max=0,max1=0;      scanf("%d%d",&n,&m);      for(i=0;i<n;i++)      {           for(j=0;j<m;j++)           {                scanf("%d",&a[i][j]);           }      }
         for(j=0;j<m;j++)
         {
            sum=0;
            for(i=0;i<n;i++)
            {
                sum=sum+a[i][j]; 
            }
            printf("%d ",sum);
         }
    }

--- Column_wise_sum_of_a_matrix.c
+++ Column_wise_sum_of_a_matrix.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main()
 {
-     int a[100][100],i,n,m,j,sumr,sumc;
-     scanf("%d%d",&m,&n);
-     for(i=0;i<m;i++)
+     int a[100][100],i,n,m,j,sum=0,max=0,max1=0;
+     scanf("%d%d",&n,&m);
+     for(i=0;i<n;i++)
      {
-          for(j=0;j<n;j++)
+          for(j=0;j<m;j++)
           {
                scanf("%d",&a[i][j]);
           }
      }      
-     for(i=0;i<n;i++)
-     {
-        sumc=0;
-        for(j=0;j<m;j++)
-        {
-            sumc=sumc+a[j][i]; 
-        }
-     printf("%d ",sumc);  
-     } 
      
+     for(j=0;j<m;j++)
+     {
+        sum=0;
+        for(i=0;i<n;i++)
+        {
+            sum=sum+a[i][j]; 
+        }
+        printf("%d ",sum);
+     }
 }
